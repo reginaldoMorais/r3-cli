e311b59a0de6ab5091d7be83b48e204a038c3627
Reginaldo Morais <reginaldo.cmorais@gmail.com>
2019-06-22 18:01:55 -0300

    Removing redundant exception

--- example/source/server/exceptions/BadRequestException.1.js
+++
@@ -1,9 +0,0 @@
-const red = require('colors/safe').red;
-
-class BadRequestException {
-  constructor(err) {
-    console.error(red('BadRequestException: ', err));
-  }
-}
-
-module.exports = BadRequestException;
